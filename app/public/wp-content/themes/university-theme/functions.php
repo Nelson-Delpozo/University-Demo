67d726bc9e8b5d4f71264969e50984f186a3bd78
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-31 20:51:28 -0700

    updated php image paths

--- app/public/wp-content/themes/university-theme/functions.php
+++ app/public/wp-content/themes/university-theme/functions.php
@@ -1,15 +1,15 @@
 <?php
 
 
-function university_demo_files(): void
+function university_files(): void
 {
+
     wp_enqueue_style('main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('extra_styles', get_theme_file_uri('/build/index.css'));
-    wp_enqueue_style('font_awesome', get_theme_file_uri('//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'));
-    wp_enqueue_style('google_fonts', get_theme_file_uri('//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i'));
-
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('google_fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 }
 
 
 
-add_action('wp_enqueue_scripts', 'university_demo_files');
+add_action('wp_enqueue_scripts', 'university_files');
