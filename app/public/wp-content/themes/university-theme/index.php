c958702bfa657ad3fa1b1af8d616ee822f947c0c
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-31 19:17:44 -0700

    added header and footer

--- app/public/wp-content/themes/university-theme/index.php
+++ app/public/wp-content/themes/university-theme/index.php
@@ -1,5 +1,5 @@
 <?php
-
+get_header();
 //function sayHello($name, $color): void
 //{
 //    echo "<p>Hello, my name is $name, and my favorite color is $color</p>";
@@ -7,9 +7,9 @@
 //sayHello("bob", "red");
 //
 //?>
-<!---->
-<!--<h1>--><?php //bloginfo('description'); ?><!--</h1>-->
-<!---->
+    <!---->
+    <!--<h1>--><?php //bloginfo('description'); ?><!--</h1>-->
+    <!---->
 <?php
 //$names = array("Me", "You", "BooBoo", "Kitty");
 //
@@ -19,12 +19,14 @@
 //    $count++;
 //}
 
-    while(have_posts()){
-        the_post(); ?>
-        <h2><a href="<?php the_permalink(); ?> "><?php the_title() ?></a></h2>
-        <?php the_content() ?>
-        <hr>
-<?php
+while (have_posts()) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?> "><?php the_title() ?></a></h2>
+    <?php the_content() ?>
+    <hr>
+    <?php
 
 
-    }
+}
+get_footer();
+?>
