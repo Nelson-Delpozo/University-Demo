fe7e0712f36ca4950c31a41c5eb543adea51305a
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-31 20:57:49 -0700

    added js/jquery

--- app/public/wp-content/themes/university-theme/functions.php
+++ app/public/wp-content/themes/university-theme/functions.php
@@ -8,6 +8,7 @@ function university_files(): void
     wp_enqueue_style('extra_styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('google_fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_script('main_university_js', get_theme_file_uri('/build/index.js'), array('jquery'), 1.0, true);
 }
 
 
