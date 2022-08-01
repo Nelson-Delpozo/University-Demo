2d0aef97738fa9caaefadac67b070152324e4364
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-31 21:35:42 -0700

    added dynamic breadcrumbs in child pages

--- app/public/wp-content/themes/university-theme/functions.php
+++ app/public/wp-content/themes/university-theme/functions.php
@@ -10,7 +10,11 @@ function university_files(): void
     wp_enqueue_style('google_fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_script('main_university_js', get_theme_file_uri('/build/index.js'), array('jquery'), 1.0, true);
 }
-
-
-
 add_action('wp_enqueue_scripts', 'university_files');
+
+
+function university_features(): void{
+    add_theme_support('title-tag');
+
+}
+add_action('after_setup_theme', 'university_features');
