c4f53ecf559f4ba85ac1ac5b913043c66bdf3762
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-31 19:01:54 -0700

    added test posts

--- app/public/wp-content/themes/university-theme/index.php
+++ app/public/wp-content/themes/university-theme/index.php
@@ -1,20 +1,30 @@
 <?php
 
-function sayHello($name, $color): void
-{
-    echo "<p>Hello, my name is $name, and my favorite color is $color</p>";
-}
-sayHello("bob", "red");
-
-?>
-
-<h1><?php bloginfo('description'); ?></h1>
-
+//function sayHello($name, $color): void
+//{
+//    echo "<p>Hello, my name is $name, and my favorite color is $color</p>";
+//}
+//sayHello("bob", "red");
+//
+//?>
+<!---->
+<!--<h1>--><?php //bloginfo('description'); ?><!--</h1>-->
+<!---->
 <?php
-$names = array("Me", "You", "BooBoo", "Kitty");
+//$names = array("Me", "You", "BooBoo", "Kitty");
+//
+//$count = 0;
+//while($count < count($names)){
+//    echo "<h2>Hello, $names[$count]</h2>";
+//    $count++;
+//}
 
-$count = 0;
-while($count < count($names)){
-    echo "<h2>Hello, $names[$count]</h2>";
-    $count++;
-}
+    while(have_posts()){
+        the_post(); ?>
+        <h2><?php the_title() ?></h2>
+        <?php the_content() ?>
+        <hr>
+<?php
+
+
+    }
