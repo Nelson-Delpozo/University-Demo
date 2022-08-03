e3982574a3bfdef8001a5f08abb835ea371dab23
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-08-03 13:50:24 -0500

    safety push

--- app/public/wp-content/themes/university-theme/single.php
+++ app/public/wp-content/themes/university-theme/single.php
@@ -20,7 +20,7 @@ while (have_posts()) {
         <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
                 <a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>"><i
-                            class="fa fa-home" aria-hidden="true"></i> Back to Blog</a> <span class="metabox__main"><?php the_title(); ?></span>
+                            class="fa fa-home" aria-hidden="true"></i> Back to Blog</a> <span class="metabox__main">posted by: <?php the_author_posts_link(); ?> on <?php the_time('n.j.y') ?> in <?php echo get_the_category_list(', ') ?></span>
             </p>
         </div>
         <div class="generic-content">
