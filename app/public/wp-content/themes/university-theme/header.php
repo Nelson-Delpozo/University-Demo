e28fc135c7800b7c066826d98112deaa683fb074
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-08-12 22:55:54 -0500

    homepage link to events

--- app/public/wp-content/themes/university-theme/header.php
+++ app/public/wp-content/themes/university-theme/header.php
@@ -21,7 +21,7 @@
                     if(is_page('about-us') or wp_get_post_parent_id(0) == 21) echo 'class="current-menu-item"'
                     ?>><a href="<?php echo site_url('about-us') ?>">About Us</a></li>
                     <li><a href="#">Programs</a></li>
-                    <li><a href="#">Events</a></li>
+                    <li><a href="<?php echo site_url('events') ?>">Events</a></li>
                     <li><a href="#">Campuses</a></li>
                     <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"' ?>>
                         <a href="<?php echo site_url('blog') ?>">Blog</a></li>
