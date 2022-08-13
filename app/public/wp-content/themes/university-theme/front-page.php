803b0056c3dce5a1938f8416d34d61aa66a7321a
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-08-13 16:54:15 -0500

    updated event links and navbar highlighting

--- app/public/wp-content/themes/university-theme/front-page.php
+++ app/public/wp-content/themes/university-theme/front-page.php
@@ -33,7 +33,11 @@ get_header(); ?>
                         </a>
                         <div class="event-summary__content">
                             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>;"><?php the_title() ?></a></h5>
-                            <p><?php echo wp_trim_words(get_the_content(), 20) ?>>
+                            <p><?php if(has_excerpt()){
+                                    echo get_the_excerpt();
+                                }else{
+                                    echo wp_trim_words(get_the_content(), 20);
+                                } ?>
                                 <a href="<?php the_permalink() ?>;" class="nu gray">Learn more</a></p>
                         </div>
                     </div>
@@ -67,7 +71,7 @@ get_header(); ?>
 <!--                    </div>-->
 <!--                </div>-->
 
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+                <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
             </div>
         </div>
         <div class="full-width-split__two">
